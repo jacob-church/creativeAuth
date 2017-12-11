d7e9114f078f12b64f80620273fd7b8cd5fb8894
hale38 <hale38@cs.byu.edu>
2017-12-11 02:48:54 +0000

    account creation and sign in allowed with google and email, continual sign after reloading and page changes

--- auth/public/scripts/scripts.js
+++ auth/public/scripts/scripts.js
@@ -1,3 +1,62 @@
+
+window.onload=function()
+{
+initApp();
+
+}
+
+
+
+
+
+function initApp(){
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        }
+	 else {
+	console.log('no user');
+	//window.location='/index.html';
+         };
+
+})
+}
+
+
+
+function emailSignIn(){
+console.log('in sign in')
+if(!firebase.auth().currentUser){
+firebase.auth().signInWithEmailAndPassword(document.getElementById('emailSignIn').value, document.getElementById('pwdSignIn').value).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+alert(errorCode)  // ...
+});}
+else{
+firebase.auth().signOut();
+console.log('out')
+}
+
+}
+
+
+function emailSignUp()
+{
+if(!firebase.auth().currentUser)
+{firebase.auth().createUserWithEmailAndPassword(document.getElementById('email').value, document.getElementById('pwd').value).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  alert(errorMessage);// ...
+});
+}
+else{
+firebase.auth().signOut();
+console.log('out')
+}
+}
+
+
 function googleSign()
 {
 if(!firebase.auth().currentUser){
@@ -22,44 +81,6 @@ console.log(errorCode)
 });
 
 }
-
-else
-{
-
-firebase.auth().signOut();
-console.log('out')
-}
-}
-
-
-
-
-
-
-
-function fbSign()
-{
-if(!firebase.auth().currentUser){
-var provider = new firebase.auth.FacebookAuthProvider();
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-}
-
 else
 {
 
