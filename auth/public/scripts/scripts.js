4920e934f37dd9b3ebbadb0fddebfa32a840c663
Jacob Church <c.jacob.church@gmail.com>
2017-12-11 19:56:20 +0000

    the club is cool now

--- auth/public/scripts/scripts.js
+++ auth/public/scripts/scripts.js
@@ -64,32 +64,31 @@ console.log('out')
 
 function googleSign()
 {
-if(!firebase.auth().currentUser){
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-console.log(user);;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-var errorCode = error.code;
-console.log(errorCode)
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
+  if(!firebase.auth().currentUser){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      console.log(errorCode)
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
 }
 else
 {
-
-firebase.auth().signOut();
-console.log('out')
+  firebase.auth().signOut();
+  console.log('out')
 }
 }
